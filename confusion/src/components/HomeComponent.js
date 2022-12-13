21f864850e75c55175fdd44a4e254c426dd5908f
agarwalrahul-22 <ag.rahul223@gmail.com>
2022-12-13 15:38:23 +0530

    netlify

--- confusion/src/components/HomeComponent.js
+++ confusion/src/components/HomeComponent.js
@@ -5,8 +5,9 @@ import { baseUrl } from '../shared/baseUrl';
 import { FadeTransform } from 'react-animation-components';
 
 function RenderCard(item,isLoading, errMess ) {
-    // console.log(item);
-    if (isLoading) {
+    // console.log(item.item);
+    // console.log(baseUrl + item?.item?.image);
+    if (!isLoading) {
         return <Loading />;
     }
     else if (errMess) {
@@ -15,21 +16,27 @@ function RenderCard(item,isLoading, errMess ) {
         );
     }
     else return (
-        <FadeTransform in transformProps={{
-            exitTransform: 'scale(0.5) translateY(-50%)'
-        }}>
-            <Card>
-                <CardImg src={baseUrl + item.image} alt={item.name} />
-                <CardBody>
-                    <CardTitle>{item.name}</CardTitle>
-                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
-                    <CardText>{item.description}</CardText>
-                </CardBody>
-            </Card>
-        </FadeTransform>
-        );
+      <FadeTransform
+        in
+        transformProps={{
+          exitTransform: "scale(0.5) translateY(-50%)",
+        }}
+      >
+        <Card>
+          <CardImg src={baseUrl + item?.item?.image} alt={item?.item?.name} />
+          <CardBody>
+            <CardTitle>{item?.item?.name}</CardTitle>
+            {item?.item?.designation ? (
+              <CardSubtitle>{item?.item?.designation}</CardSubtitle>
+            ) : null}
+            <CardText>{item?.item?.description}</CardText>
+          </CardBody>
+        </Card>
+      </FadeTransform>
+    );
     }
 function Home(props) {
+    // console.log(props);
     return (
         <div className="container">
                     <div className="row align-items-start">
@@ -40,7 +47,6 @@ function Home(props) {
                             <RenderCard item={props.promotion} isLoading={props.promosLoading} errMess={props.promosErrMess} />
                         </div>
                         <div className="col-12 col-md m-1">
-                            
                             <RenderCard item={props.leader} isLoading={props.leadersLoading} errMess={props.leadersErrMess} />
                         </div>
                     </div>
--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -40,7 +40,6 @@ class Main extends Component {
     this.props.fetchPromos();
     this.props.fetchLeaders();
   }
-  
 
   onDishSelect(dishId) {
     this.setState({ selectedDish: dishId});
@@ -48,17 +47,25 @@ class Main extends Component {
 
   render() {
     const HomePage = () => {
-      console.log(this.props);
-      return(
-          <Home 
+      // console.log(this.props);
+      return (
+        <Home
           dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
           dishesLoading={this.props.dishes.isLoading}
           dishErrMess={this.props.dishes.errMess}
-          promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+          promotion={
+            this.props.promotions.promotions.filter(
+              (promo) => promo.featured
+            )[0]
+          }
           promoLoading={this.props.promotions.isLoading}
           promoErrMess={this.props.promotions.errMess}
-          leader={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
-         />
+          leader={
+            this.props.leaders.leaders.filter((leader) => leader.featured)[0]
+          }
+          leadersLoading={this.props.leaders.isLoading}
+          leadersErrMess={this.props.leaders.errMess}
+        />
       );
     }
 
@@ -76,7 +83,7 @@ class Main extends Component {
       />
       );
     };
-
+// console.log(this.props);
     return (
       <div>
         <Header />
